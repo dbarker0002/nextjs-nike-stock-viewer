17bde61b869b07858f731a026aae0f519b4441b7
dbarker0002 <dbarker0002@gmail.com>
2023-12-20 20:32:29 -0800

    Add CSV exporting and change filter placeholders

--- src/components/gridContainer.tsx
+++ src/components/gridContainer.tsx
@@ -11,6 +11,8 @@ import { BsCartCheck } from "react-icons/bs";
 import { FaRegCheckCircle } from "react-icons/fa";
 import { RiComputerLine } from "react-icons/ri";
 import { HiDevicePhoneMobile } from "react-icons/hi2";
+import { FaAngleDown } from "react-icons/fa";
+import { GrDocumentDownload } from "react-icons/gr";
 import { Input } from "@/components/ui/input"
 import { Separator } from "@/components/ui/separator"
 
@@ -76,24 +78,39 @@ export default function GridContainer({products}: {products:ProductData[]}) {
         );
     });
 
+    const handleExportCSV = () => {
+        const csvData = [
+            Object.keys(cards[0]).filter(key => key !== "sizes"), // Title row
+            ...cards.map(card => Object.values(card).filter((_, index) => Object.keys(card)[index] !== "sizes")) // Data rows
+        ];
+        const csvContent = "data:text/csv;charset=utf-8," + csvData.map(row => row.join(",")).join("\n");
+        const encodedUri = encodeURI(csvContent);
+        const link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "cards.csv");
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+    }
+
     return (
         <div className="w-full">
             <div className="mx-4 my-4 space-x-1 flex flex-wrap justify-center items-center">
-                <h1 className="flex-shrink-0 mr-6 mb-2">Nike Stock Viewer</h1>
+                <h1 className="flex-shrink-0 mr-6 mb-2">Nike Manager</h1>
                 <div className="flex-grow">
                 <   Input placeholder="Filter Products" className="w-full focus:ring-0"  
                     onChange={(e) => setSearchValue(e.target.value)} value={searchValue}/>
                 </div>
                 {/* <div className="space-x-1"> */}
-                    <Button variant="outline"><FaRegCheckCircle className="mr-1"/>Active</Button>
-                    <Button variant="outline"><BsCartCheck className="mr-1"/>In Stock</Button>
-                    <Button variant="outline"><RiComputerLine className="mr-1"/>Nike.com</Button>
-                    <Button variant="outline"><HiDevicePhoneMobile className="mr-1"/>SNKRS</Button>
+                    <Button variant="outline"><FaRegCheckCircle className="mr-1"/>Status<FaAngleDown className="ml-1"/></Button>
+                    <Button variant="outline"><BsCartCheck className="mr-1"/>Availability<FaAngleDown className="ml-1"/></Button>
+                    <Button variant="outline"><RiComputerLine className="mr-1"/>Platform<FaAngleDown className="ml-1"/></Button>
                 {/* </div> */}
                 <Separator orientation="vertical" className="h-[34px]" style={{marginLeft: "12px", marginRight: "8px"}} />
                 {/* <div className="space-x-1"> */}
                     <AddModal onAdd={handleAddCard}></AddModal>
                     <Button variant="outline" onClick={() => handleRefresh()}><HiOutlineRefresh className="mr-1"/>Refresh</Button>
+                    <Button variant="outline" onClick={handleExportCSV}><GrDocumentDownload className="mr-1"/>Export CSV</Button>
                 {/* </div> */}
             </div>
             <div><hr className="pb-2 pt-0 mx-4"></hr></div>
