155b9b15687e746f4fb5c379eefa31680483cf58
dbarker0002 <dbarker0002@gmail.com>
2023-12-20 16:10:30 -0800

    remove tech fleece from default items

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GridContainer from "@/components/gridContainer"
 import { getProductData } from "@/utils/nike/product";
 
 export default async function Home() {
-    let skus = ['CT8012-170', 'FV4921-600', 'FB7921-060', 'AR0715-200', 'DO7097-100', 'DD1391-100', '553560-030', 'DC0350-100', 'CZ0790-101'];
+    let skus = ['CT8012-170', 'FV4921-600', 'AR0715-200', 'DO7097-100', 'DD1391-100', '553560-030', 'DC0350-100', 'CZ0790-101'];
     
     let products = await getProductData(skus);
 
