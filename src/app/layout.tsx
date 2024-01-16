d95275776426d1de4f7ea80fc1b52fb7cf5ed72e
dbarker0002 <dbarker0002@gmail.com>
2024-01-16 04:38:52 -0800

    Add clerk routes and middleware

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 
+import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/toaster"
 import { ThemeProvider } from '@/components/theme-provider'
 
@@ -32,18 +33,20 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>
-         <ThemeProvider
-            attribute="class"
-            defaultTheme="light"
-            enableSystem
-            disableTransitionOnChange
-          >
-        {children}
-        <Toaster />
-        </ThemeProvider>
-      </body>
-    </html>
+    <ClerkProvider>
+        <html lang="en">
+            <body className={inter.className}>
+                <ThemeProvider
+                    attribute="class"
+                    defaultTheme="light"
+                    enableSystem
+                    disableTransitionOnChange
+                >
+                    {children}
+                    <Toaster />
+                </ThemeProvider>
+            </body>
+        </html>
+    </ClerkProvider>
   )
 }
